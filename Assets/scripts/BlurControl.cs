493e07783ef54caac7b1165fdc6d4dafa20bede3
rng74 <ers98ers@gmail.com>
2018-11-12 22:26:01 +0600

    Added dynamic blur based on width of device

---
+++ Assets/scripts/BlurControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlurControl : MonoBehaviour {
+    public RawImage img;
+	void Start () {
+        float resolution = Screen.width/130.0f;
+        img.material.SetFloat("_Size", resolution);
+	}
+}
